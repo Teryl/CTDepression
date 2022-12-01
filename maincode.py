3e84d0a4d155587fd12c322080e9218013240f17
xenaria <danthemarithe@hotmail.com>
2022-12-02 07:06:46 +0800

    test code skel

--- maincode.py
+++ maincode.py
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
 import math
 import random
 import time
+
 while True:  
     ## set global stage and time
     globalStage = 1
@@ -10,17 +10,18 @@ while True:
     timeRemaining = 10
 
     ## initialize enemy dict and statlist
-    enemyDict = {"HP": 0, "Atk": 0, "Def": 0, "Time": 0, "globalNRange": 0}
+    # for now initialize HP and attack as 100 and 50, later change depending on enemy and player3 type
+    enemyDict = {"HP": 100, "Atk": 50, "Def": 0, "Time": 0, "globalNRange": 0}
     enemyStatlist = {
         "HP":{0:25,1:45,2:80},
-        "Atk":{0:0.75,1:1,2,2:1.25}, 
-        "Def":{0:1.25,1:1.2,2:0.75}, 
-        "Time": {0:10,1:15,2:20}, 
+        "Atk":{0:0.75,1:1,2:1.25},
+        "Def":{0:1.25,1:1.2,2:0.75},
+        "Time": {0:10,1:15,2:20},
         "globalNRange": {0:[0,10], 1:[5,25], 2:[25,100], 3:[100,1000]}
         }
     
     ## initialize player dict and statlist 
-    playerDict = {"HP": 0, "Atk": 0, "Def": 0, "Luck": 0, "Time": 0}
+    playerDict = {"HP": 100, "Atk": 50, "Def": 0, "Luck": 0, "Time": 0}
     playerStatlist = {
         "HP":{0:15,1:25,2:45,3:65,4:90,5:120},
         "Atk":{0:1,1:1.2,2:1.4,3:1.6,4:1.8,5:2}, 
@@ -29,6 +30,7 @@ while True:
         "Luck": {0:100, 1:98, 2:96, 3:94, 4:92, 5:90}
         }
     
+    ## hardcode HP for now
     enemyHP = enemyDict["HP"]
     playerHP = playerDict["HP"]
     
@@ -36,24 +38,48 @@ while True:
     # while player and enemy HP is greater than 0, round continues
     # when HP reaches 0 for any entity, exit while loop and increase stage
     while playerHP > 0 and enemyHP > 0:
-        # generate new number n based on enemy 
-        # for now value will equate to 1st index in the list, 10
+        # generate new number n based on enemy
+        # for now value will equate to 1st index in the list, 25
         n = enemyStatlist['globalNRange'][globalStage][1]
-
-        equation = input("Write your equation here: ")
         
+        print("Calculator value:",n)
+        print("Player HP: {}\nEnemy HP: {}".format(playerHP,enemyHP))
+
+        ## initialize first damage as 0
+        playerDmg = 0
+        enemyDmg = 0
+        print("Player Dmg: {}\nEnemy Dmg: {}".format(playerDmg,enemyDmg))
+
+        equation = eval(input("Write your equation here: "))
+        print(equation)
+        ## crit, multiplier hard coded for now
+        crit = 1
+        multiplier = 1.2
+
+        # if player types wrong eqn, damage dealt to player
         if equation != n:
-            playerDmg = 0
+            enemyDmg = enemyDict["Atk"] * globalStage**(multiplier)
+            print("Damage dealt by enemy: ",enemyDmg)
+            playerHP = playerHP - enemyDmg
+        
+        # if player types right eqn, damage dealt to enemy
+        else:
 
-        #playerDmg = 
-        enemyDmg = enemyDict["Atk"] * globalStage**(multiplier)
-        finalDamage = playerDmg - enemyDmg
+            playerDmg = playerDict["Atk"] * globalStage**(multiplier)
+            print("Damage dealt by player: ",playerDmg)
+            enemyHP = enemyHP - playerDmg
+        
+            
 
-        if finalDamage > 0:
+            
+        
+        finalDmg = playerDmg - enemyDmg
+        print("New Player HP: {}\nNew Enemy HP: {}".format(playerHP,enemyHP))
+        if finalDmg > 0:
             dmgToEnemy = crit * finalDmg * enemyDict["Def"]
             dmgToPlayer = 0
 
-        if finalDamage < 0:
+        if finalDmg < 0:
             dmgToPlayer = finalDmg *  playerDict["Def"]
             dmgToEnemy = 0
     
@@ -70,11 +96,3 @@ while True:
         pass
     
     pass
-=======
-import math
-import random
-while(True):
-    globalTime = 0
-    globalStage = 1
-       
->>>>>>> c3d3958d30f75a62dfb6befdb6d755cf027aeec3
